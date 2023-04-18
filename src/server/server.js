1e2e20f3d0d3bc50e3564fa0de1b93607255cc1e
Greg Bowne <gbowne144@gmail.com>
2023-04-17 17:13:52 -0700

    Fixing up the routes in the /src/server.js file as well cleaning up the server.js file itself.

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,94 @@
-// Import required modules
 const express = require('express');
-const mongoose = require('mongoose');
+const session = require('express-session');
 const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-// Create an Express app
+
 const app = express();
 
-// Set up mongoose connection
-mongoose.set("strictQuery", false);
-const mongoDB = "insert_your_database_url_here";
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
+// Middleware setup
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Database connection
+mongoose.connect('your-mongodb-connection-string', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Set view engine to HTML
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../client'));
+
+// Routes
+app.get('/dashboard', (req, res) => { /* ... */ });
+app.get('/contestlog', (req, res) => { /* ... */ });
+app.get('/scanlog', (req, res) => { /* ... */ });
+app.get('/swllog', (req, res) => { /* ... */ });
+app.get('/signin', (req, res) => { /* ... */ });
+app.get('/hamlog', (req, res) => { /* ... */ });
+app.get('/signup', (req, res) => { /* ... */ });
+app.get('/mwlog', (req, res) => { /* ... */ });
+app.get('/vhflog', (req, res) => { /* ... */ });
+app.get('/signin/login', (req, res) => { /* ... */ });
+app.get('/auth', (req, res) => { /* ... */ });
+app.get('/logout', (req, res) => { /* ... */ });
+
+// User schema for demonstration purposes
+const users = [];
+
+app.post('/auth', async (req, res) => {
+	const user = users.find(u => u.email === req.body.email);
+	if (user == null) {
+	  return res.status(400).send('User not found');
+	}
+
+	try {
+	  if (await bcrypt.compare(req.body.password, user.password)) {
+		const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+		res.json({ accessToken: accessToken });
+	  } else {
+		res.status(401).send('Invalid password');
+	  }
+	} catch {
+	  res.status(500).send('Error during authentication');
+	}
 });
 
-// Start the server on a specified port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port \${PORT}`);
+app.post('/signup', async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const user = { email: req.body.email, password: hashedPassword };
+		users.push(user);
+		res.status(201).send('User created');
+	  } catch {
+		res.status(500).send('Error creating user');
+	  }
+});
+
+// 404 Not Found
+app.use('*', (req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+// Listen on port 3000 or the next available port
+const server = app.listen(3000, () => {
+  console.log(`Server is listening on port: \${server.address().port}`);
 });
