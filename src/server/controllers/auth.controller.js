2ab3a8c489528f44f79672396c39ec34ea63edaf
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2023-04-25 14:57:02 -0700

    Feat(Update controller)

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -1,5 +1,10 @@
-module.signin = (req, res, next) {
-
+module.auth = async (req, res, next) => {
+  try {
+    const {user, password} = req.body;
+    const data = await auth(user, password);
+  } catch(err) {
+    next(boom.badRequest("Wrong username or password"));
+  }
 }
 
 
