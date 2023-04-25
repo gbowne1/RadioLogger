71a8fbaae118bcd7c1af4cb4dea3c8b848cad33e
Greg Bowne <gbowne144@gmail.com>
2023-04-24 19:38:47 -0700

    This commit:
    - Initial work for the user profile at /profile as profile.html and includes the css file
    - adds DOM stuff to index.js
    - re-Prettified dashboard.css

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Your initialization code here, e.g., setting up event listeners
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  button.addEventListener('click', () => {
+    // Your code here, e.g., redirecting to the login page
+    window.location.href = '/login/login.html';
+  });
 });
