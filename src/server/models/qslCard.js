df34906682a3f0bb5b560616f4aada36a8e972f6
Greg Bowne <gbowne144@gmail.com>
2023-04-28 14:49:48 -0700

    This commit:
    - Adds a reuseable web component called navbar, this will replace the navbar across all the pages.
    - Adds a qslCard model schema for the qsllog page database.
    - Tried to fix some issues in dashboard.css  with the webkit and moz.

---
+++ src/server/models/qslCard.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const qslCardSchema = new mongoose.Schema({
+  callsign: {
+    type: String,
+    required: true
+  },
+  dateSent: {
+    type: Date,
+    required: true
+  },
+  dateReceived: {
+    type: Date,
+    required: true
+  },
+  frequency: {
+    type: Number,
+    required: true
+  },
+  mode: {
+    type: String,
+    required: true
+  },
+  qslSent: {
+    type: Boolean,
+    default: false
+  },
+  qslReceived: {
+    type: Boolean,
+    default: false
+  },
+  notes: {
+    type: String
+  }
+});
+
+const QslCard = mongoose.model('QslCard', qslCardSchema);
