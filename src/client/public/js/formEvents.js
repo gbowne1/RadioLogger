832c74eacd4d8b486674ba84f8cadd321c6c5961
Greg Bowne <gbowne144@gmail.com>
2023-05-03 14:29:45 -0700

    This commit fixes most of the CDN problems by copying the working CDN's scripts and link tags from dashboard.html

--- src/client/public/js/formEvents.js
+++ src/client/public/js/formEvents.js
@@ -2,6 +2,8 @@ document.querySelector('#p1').addEventListener('change', function () {
   document.querySelector('#p2').pattern = this.value;
 });
 
-document.querySelector('form[action="signUp.js"]').addEventListener('submit', function (e) {
-  checkpass();
-});
+function checkpass() {
+  document.querySelector('form[action="signUp.js"]').addEventListener('submit', function (e) {
+    checkpass();
+  });
+}
