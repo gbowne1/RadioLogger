31a69c69e2be1d86b68f160db98dde62ca683fc9
Gregory Bowne <gbowne144@gmail.com>
2025-11-30 09:49:14 -0800

    Refactor scanLog model to use external schema

--- src/server/models/scanlog.model.js
+++ src/server/models/scanlog.model.js
@@ -1,59 +1,6 @@
 const mongoose = require('mongoose');
 
-const scanLogSchema = new mongoose.Schema({
-  frequency: {
-    type: Number,
-    required: true
-  },
-  callSign: {
-    type: String,
-    required: true
-  },
-  tone: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  mode: {
-    type: String,
-    required: true
-  },
-  toneType: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  systemType: {
-    type: String,
-    required: true
-  },
-  systemId: {
-    type: String,
-    required: true
-  },
-  talkgroups: {
-    type: String,
-    required: true
-  },
-  tag: {
-    type: String,
-    required: true
-  },
-  systemFrequencies: {
-    type: String,
-    required: true
-  },
-  repeaters: {
-    type: String,
-    required: true
-  }
-});
+const scanLogSchema = require('../schemas/scanlog.schema');
 
 const ScanLog = mongoose.model('ScanLog', scanLogSchema);
 
