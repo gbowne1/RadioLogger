f5389f37aa8047ef4f745be97cd60a7d488e60fd
Gregory Bowne <gbowne144@gmail.com>
2025-11-30 09:51:24 -0800

    Add userId field to scan log schema

--- src/server/schemas/scanlog.schema.js
+++ src/server/schemas/scanlog.schema.js
@@ -1,60 +1,63 @@
 const mongoose = require('mongoose');
 
 const scanLogSchema = new mongoose.Schema({
-  frequency: {
-    type: Number,
-    required: true
-  },
-  callSign: {
-    type: String,
-    required: true
-  },
-  tone: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  mode: {
-    type: String,
-    required: true
-  },
-  toneType: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  systemType: {
-    type: String,
-    required: true
-  },
-  systemId: {
-    type: String,
-    required: true
-  },
-  talkgroups: {
-    type: String,
-    required: true
-  },
-  tag: {
-    type: String,
-    required: true
-  },
-  systemFrequencies: {
-    type: String,
-    required: true
-  },
-  repeaters: {
-    type: String,
-    required: true
-  }
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User' 
+    },
+    frequency: {
+        type: Number,
+        required: true
+    },
+    callSign: {
+        type: String,
+        required: true
+    },
+    tone: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    mode: {
+        type: String,
+        required: true
+    },
+    toneType: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    systemType: {
+        type: String,
+        required: true
+    },
+    systemId: {
+        type: String,
+        required: true
+    },
+    talkgroups: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        required: true
+    },
+    systemFrequencies: {
+        type: String,
+        required: true
+    },
+    repeaters: {
+        type: String,
+        required: true
+    }
 });
 
-const ScanLog = mongoose.model('ScanLog', scanLogSchema);
-
-module.exports = ScanLog;
+module.exports = scanLogSchema;
