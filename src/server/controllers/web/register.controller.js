904c9fff0b8b28e4a9a3482e31a5fd70daa7ced5
Gregory Bowne <gbowne144@gmail.com>
2026-02-23 14:17:57 -0800

    Update register.controller.js
    Eslint fix

--- src/server/controllers/web/register.controller.js
+++ src/server/controllers/web/register.controller.js
@@ -1,6 +1,3 @@
-const boom = require('@hapi/boom');
-const logger = require('../../utils/logger');
-
-exports.show = (req, res, next) => {
-  res.render('register');
-};
+exports.show = (req, res) => {
+  res.render('register')
+}
