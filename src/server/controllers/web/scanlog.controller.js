52986b47a54a6c72964218b7b9d0be6cbbc144a7
Gregory Bowne <gbowne144@gmail.com>
2026-02-23 14:25:52 -0800

    Update scanlog.controller.js

--- src/server/controllers/web/scanlog.controller.js
+++ src/server/controllers/web/scanlog.controller.js
@@ -1,6 +1,3 @@
-const boom = require('@hapi/boom');
-const logger = require('../../utils/logger');
-
-exports.show = (req, res, next) => {
-  res.render('scanlog/scanlog');
-};
+exports.show = (req, res) => {
+  res.render('scanlog/scanlog')
+}
