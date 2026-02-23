4c0651fdf515b513b0c32bdfe1ce6f7729102e38
Gregory Bowne <gbowne144@gmail.com>
2026-02-23 14:51:03 -0800

    Update auth.service.js

--- src/server/services/auth.service.js
+++ src/server/services/auth.service.js
@@ -1,11 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const { API_SECRET } = require('../config/secrets');
 
 exports.login = async (username, password) => {
-  const user = await User.findOne({
-    username: username
-  });
+  const user = await User.findOne({ username });
 
   console.log('auth.service login', username, password, 'found', user);
 
@@ -14,31 +11,29 @@ exports.login = async (username, password) => {
     console.log('all', all);
     throw new Error('Authentication failed. Invalid user or password');
   }
-  return (user)
-}
+
+  return user;
+};
 
 exports.find = async (username) => {
-  const user = await User.findOne({
-    username: username
-  });
+  const user = await User.findOne({ username });
 
   if (!user) {
     throw new Error('Email not exists');
   }
 
   return user;
-}
+};
 
 exports.resetPassword = async (username, oldPassword, newPassword) => {
-  const user = await User.findOne({
-    username: username
-  });
+  const user = await User.findOne({ username });
+
   if (!user || !user.comparePassword(oldPassword)) {
     throw new Error('Invalid user or password');
   }
 
   user.password = bcrypt.hashSync(newPassword, 10);
-
   await user.save();
+
   return user;
-}
+};
